16dadd2e8ac147d596d14b1e7b435dba00a3364b
ReuelMB <145316314+ReuelMB@users.noreply.github.com>
2024-05-13 13:31:14 +0800

    Updated SociableKit Link

--- src/Sections/Testimonials.jsx
+++ src/Sections/Testimonials.jsx
@@ -1,7 +1,6 @@
 import SectionHead from "../Components/SectionHead";
 
 export default function Testimonials() {
-
   return (
     <div className="flex flex-col justify-center">
       <SectionHead
@@ -9,7 +8,7 @@ export default function Testimonials() {
         text="In my previous work experiences, I demonstrated the ability to work effectively both independently and as part of a team. Colleagues have consistently praised my collaborative approach."
       />
       <iframe
-        src="https://widgets.sociablekit.com/linkedin-recommendations/iframe/25410393"
+        src="https://widgets.sociablekit.com/linkedin-recommendations/iframe/25411458"
         className="w-full h-[500px]"
       ></iframe>
     </div>
